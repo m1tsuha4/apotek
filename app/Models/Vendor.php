0f4810a112369162687237c1079a5370ffc58425
m1tsuha4 <114287774+m1tsuha4@users.noreply.github.com>
2024-06-24 18:48:52 +0700

    upd vendor

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Vendor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nama_vendor',
+        'perusahaan',
+        'no_telepon',
+        'alamat'
+    ];
 }

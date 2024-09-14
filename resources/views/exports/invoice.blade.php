29e4a1b649f5cc4091e80d69cdabc1589222838e
m1tsuha4 <114287774+m1tsuha4@users.noreply.github.com>
2024-09-14 19:57:53 +0700

    upd invoice

--- resources/views/exports/invoice.blade.php
+++ resources/views/exports/invoice.blade.php
@@ -29,16 +29,59 @@
             text-align: left;
             padding: 2px; 
         }
+
+        @media print {
+
+            @page {
+                size: letter portrait;
+                margin: 0.5cm; 
+            }
+
+
+            body {
+                width: 100%;
+                overflow: visible;
+            }
+
+  
+            body, .container {
+                padding: 0;
+                margin: 0;
+                width: 100%;
+            }
+
+       
+            .table {
+                width: 100%;
+                border-collapse: collapse;
+            }
+
+            .table thead th, 
+            .table tbody td {
+                padding: 5px;
+                font-size: 12px;
+            }
+
+     
+            .p-5 {
+                padding: 0 !important;
+            }
+
+     
+            .invoice-title {
+                font-size: 1.3rem;
+            }
+        }
     </style>
 </head>
 
-<body style="width: 1200px;" class="p-5">
-    <div class="container">
+<body class="p-5">
+    <div class="">
         <div class="row mb-4">
-            <div class="col-1 d-flex align-items-center">
+            <div class="col-2 d-flex align-items-center">
                 <h1 class="invoice-title">Invoice</h1>
             </div>
-            <div class="col-11">
+            <div class="col-10">
                 <table class="table table-bordered">
                     <thead>
                         <tr>
@@ -77,21 +120,21 @@
                     </thead>
                     <tbody>
                         @php
-                        $no = 1;
-                    @endphp
-                    @foreach ($data['barangPenjualan'] as $item)
-                        <tr>
-                            <td class="text-center px-2">{{ $no++ }}</td>
-                            <td class="text-center px-2">{{ $item['nama_barang'] }}</td>
-                            <td class="text-center px-2">{{ $item['batch'] }}</td>
-                            <td class="text-center px-2">{{ $item['exp_date'] }}</td>
-                            <td class="text-end px-2">{{ $item['jumlah'] }}</td>
-                            <td class="px-2">{{ $item['nama_satuan'] }}</td>
-                            <td class="text-end px-2">Rp {{ number_format($item['harga'], 0, ',', '.') }}</td>
-                            <td class="text-end px-2">Rp {{ number_format($item['total_barang'], 0, ',', '.') }}</td>
-                        </tr>
+                            $no = 1;
+                        @endphp
+                        @foreach ($data['barangPenjualan'] as $item)
+                            <tr>
+                                <td class="text-center px-2">{{ $no++ }}</td>
+                                <td class="text-center px-2">{{ $item['nama_barang'] }}</td>
+                                <td class="text-center px-2">{{ $item['batch'] }}</td>
+                                <td class="text-center px-2">{{ $item['exp_date'] }}</td>
+                                <td class="text-end px-2">{{ $item['jumlah'] }}</td>
+                                <td class="px-2">{{ $item['nama_satuan'] }}</td>
+                                <td class="text-end px-2">Rp {{ number_format($item['harga'], 0, ',', '.') }}</td>
+                                <td class="text-end px-2">Rp {{ number_format($item['total_barang'], 0, ',', '.') }}</td>
+                            </tr>
+                        @endforeach
                     </tbody>
-                    @endforeach
                 </table>
             </div>
         </div>
@@ -120,21 +163,10 @@
                                 </div>  
                             </td>
                         </tr>
-                        
                     </tbody>
                 </table>
             </div>
         </div>
-
-        <!-- Notes Section -->
-        <!-- <div class="row mt-4">
-            <div class="col-12">
-                <div class="border p-3">
-                    <p><strong>Catatan:</strong></p>
-                    <p>Barang yang sudah dibeli <strong>tidak dapat ditukar atau dikembalikan</strong>, kecuali barang expired sesuai dengan ketentuan BPOM dan Apotek Semoga Jaya.</p>
-                </div>
-            </div>
-        </div> -->
     </div>
 
     <!-- Bootstrap JS -->
